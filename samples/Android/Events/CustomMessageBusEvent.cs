c15ea32d24324a61ef4eeb90f8422c335a56d856
Dave <newky2k@mac.com>
2014-05-10 22:56:40 +0100

    Added android sample
    Added android sample

---
+++ samples/Android/Events/CustomMessageBusEvent.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using DSoft.Messaging;
+
+namespace MessageBoxAndroid.Events
+{
+	/// <summary>
+	/// Custom MessageBusEvent class with extra data
+	/// </summary>
+	public class CustomMessageBusEvent : MessageBusEvent
+	{
+		public const string kCustEventId = "445566";
+
+		#region Properties
+
+		/// <summary>
+		/// Gets or sets the event identifier.
+		/// </summary>
+		/// <value>The event identifier.</value>
+		public override string EventId {
+			get
+			{
+				//return a constant eventId
+				return kCustEventId;
+			}
+		}
+
+		/// <summary>
+		/// Custom property on the event that Gets or sets the timestamp 
+		/// </summary>
+		/// <value>The time stamp.</value>
+		public DateTime TimeStamp { get; set; }
+
+		#endregion
+
+		#region Constructores
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="MessageBus_iOS.CustomMessageBusEvent"/> class.
+		/// </summary>
+		public CustomMessageBusEvent ()
+		{
+			//
+			TimeStamp = DateTime.Now;
+		}
+
+		#endregion
+	}
+}
+
---
+++ samples/Android/SecondActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MessageBoxAndroid
+{
+	[Activity (Label = "MessageBox - Sender")]			
+	public class SecondActivity : Activity
+	{
+		public const String kEventID = "123456";
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Create your application here
+		}
+	}
+}
+
